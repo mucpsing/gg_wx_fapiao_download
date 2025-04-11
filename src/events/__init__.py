fa808e31782c1cd03d4a5a246dcddef3e97cea3e
Capsion <373704015@qq.com>
2025-04-11 13:18:37 +0800

    feat: 优化事件逻辑，去除了events

--- src/events/__init__.py
+++ src/events/__init__.py
@@ -15,21 +15,40 @@ sys.path.append("..")
 
 from src.utils.tk_utils import selectPath
 from src.configEditor_launcher import UI as configEditor_show
+from src.ui.main import Application
 
 
 class UI_Var:
     Text1Var = "文件路径选择输入框，使用self.Text1Var.get()"
 
 
-class UI_Events:
+class UI_Events(Application):
     def mainBtn_sel_file_Cmd(self, event=None):
         """选择文件的按钮"""
 
         # 点击按钮打开文件的调用
-        sel_path = selectPath("file")
+        sel_path = selectPath("dir")
         if sel_path:
             if os.path.exists(sel_path):
                 self.Text1Var.set(sel_path)
+
+    def mainBtn_copy_Cmd(self, event=None):
+        scripts = """var old_open=window.open;window.__cps||(window.__cps={readEl:document.getElementsByClassName("upload_opt_icon")[0]}),window.__cps.getHtmlContent=e=>{const n=document.getElementsByClassName(e);return 1!=n.length?(console.log("发生错误了，元素获取数量不对: ",n),""):n[0].innerHTML},window.open=function(e){const n=new URL(e);n.searchParams.set("dj_ifr","false"),n.searchParams.set("open","0"),n.searchParams.set("method","download"),console.log("fdId: ",n.searchParams.get("fdId")),fetch(n,{method:"GET",credentials:"include"}).then((e=>{if(!e.ok)throw new Error(`HTTP 错误，状态码：${e.status}`);const n=e.headers.get("content-type");return n.includes("application/json")?e.json():n.includes("text/")?e.text():e.blob()})).then((e=>{const n=__cps.getHtmlContent("upload_list_filename_title")+__cps.getHtmlContent("upload_list_filename_ext"),t=new FileReader;t.onload=function(e){const t=e.target.result,o=document.createElement("a");o.href=t,o.download=n,o.style.display="none",document.body.appendChild(o),o.click(),document.body.removeChild(o)},t.readAsDataURL(e)})).catch((e=>{console.error("请求失败：",e)}))};const readEl=document.getElementsByClassName("upload_opt_icon")[0],innerEl=readEl.querySelector(".upload_opt_tip_inner");innerEl.innerHTML="下载",readEl.click();"""
+
+        import tkinter
+
+        if self.root:
+            self.root.clipboard_clear()  # 清空剪贴板
+            self.root.clipboard_append(scripts)  # 添加文本到剪贴板
+            self.root.attributes("-topmost", True)
+        else:
+            # 创建隐藏窗口
+            root = tkinter.Tk()
+            root.withdraw()  # 隐藏主窗口
+            root.clipboard_clear()  # 清空剪贴板
+            root.clipboard_append(scripts)  # 添加文本到剪贴板
+            root.update()  # 确保剪贴板更新（必须调用）
+            root.destroy()  # 销毁窗口（可选）
 
     # def mainBtn_open_config_editor_Cmd(self, subFormConfig):
     #     """打开一个子窗口"""
--- src/main_launcher.py
+++ src/main_launcher.py
@@ -10,16 +10,20 @@
 # @Description: 功能描述
 #
 
-import tkinter, os
+import tkinter, os, sys
+
+sys.path.append("..")
 
 from tkinter.messagebox import showwarning
+from src.utils.tk_utils import selectPath
 
 from src.ui.main import Application
 from src.config import Config
-from src.events import UI_Events
 
-from src.configEditor_launcher import UI as configEditor_show
-from src.configEditor_launcher import SubWindowConfig
+# from src.events import UI_Events
+
+# from src.configEditor_launcher import UI as configEditor_show
+# from src.configEditor_launcher import SubWindowConfig
 
 from enum import Enum
 
@@ -32,20 +36,22 @@ def check(config: Config):
     pass
 
 
-class UI(UI_Events, Application):
+class UI(Application):
     # 这个类实现具体的事件处理回调函数。界面生成代码在Application_ui中。
     def __init__(self, config: Config):
         check(config)
-        main_tk = tkinter.Tk()
-        super().__init__(main_tk)
+        root = tkinter.Tk()
+        super().__init__(root)
+
+        self.root = root
 
         self.master.title(config.title)
         self.config = config
 
         self.configEditor = None  # close 或者 open
 
-        screenWidth = main_tk.winfo_screenwidth()  # 获取显示区域的宽度
-        screenHeight = main_tk.winfo_screenheight()  # 获取显示区域的高度
+        screenWidth = root.winfo_screenwidth()  # 获取显示区域的宽度
+        screenHeight = root.winfo_screenheight()  # 获取显示区域的高度
         left = int((screenWidth - config.width) / 2)  # 定位x
         top = int((screenHeight - config.height) / 2 * 0.8)  # 定位y
 
@@ -70,26 +76,41 @@ class UI(UI_Events, Application):
 
         windnd.hook_dropfiles(self.master, func=dragged_files)
 
-    def mainBtn_open_config_editor_Cmd(self):
-        """打开一个子窗口"""
-        if self.configEditor is None:
-            sub_height = 500
-            subConfig = SubWindowConfig(
-                title="配置编辑器",
-                width=300,
-                height=sub_height,
-                left=int(self.master.winfo_x() + self.config.width + 5),
-                top=int(self.master.winfo_y()),
-            )
-            self.configEditor = configEditor_show(subConfig, self)
-
-        else:
-            self.configEditor.on_close()
-            self.configEditor = None
-
     def on_close(self):
         self.master.quit()
         self.master.destroy()
+
+    # def mainBtn_open_config_editor_Cmd(self):
+    #     """打开一个子窗口"""
+    #     if self.configEditor is None:
+    #         sub_height = 500
+    #         subConfig = SubWindowConfig(
+    #             title="配置编辑器",
+    #             width=300,
+    #             height=sub_height,
+    #             left=int(self.master.winfo_x() + self.config.width + 5),
+    #             top=int(self.master.winfo_y()),
+    #         )
+    #         self.configEditor = configEditor_show(subConfig, self)
+
+    #     else:
+    #         self.configEditor.on_close()
+    #         self.configEditor = None
+
+    def mainBtn_sel_file_Cmd(self, event=None):
+        """选择文件的按钮"""
+
+        # 点击按钮打开文件的调用
+        sel_path = selectPath("dir")
+        if sel_path:
+            if os.path.exists(sel_path):
+                self.Text1Var.set(sel_path)
+
+    def mainBtn_copy_Cmd(self, event=None):
+        scripts = """var old_open=window.open;window.__cps||(window.__cps={readEl:document.getElementsByClassName("upload_opt_icon")[0]}),window.__cps.getHtmlContent=e=>{const n=document.getElementsByClassName(e);return 1!=n.length?(console.log("发生错误了，元素获取数量不对: ",n),""):n[0].innerHTML},window.open=function(e){const n=new URL(e);n.searchParams.set("dj_ifr","false"),n.searchParams.set("open","0"),n.searchParams.set("method","download"),console.log("fdId: ",n.searchParams.get("fdId")),fetch(n,{method:"GET",credentials:"include"}).then((e=>{if(!e.ok)throw new Error(`HTTP 错误，状态码：${e.status}`);const n=e.headers.get("content-type");return n.includes("application/json")?e.json():n.includes("text/")?e.text():e.blob()})).then((e=>{const n=__cps.getHtmlContent("upload_list_filename_title")+__cps.getHtmlContent("upload_list_filename_ext"),t=new FileReader;t.onload=function(e){const t=e.target.result,o=document.createElement("a");o.href=t,o.download=n,o.style.display="none",document.body.appendChild(o),o.click(),document.body.removeChild(o)},t.readAsDataURL(e)})).catch((e=>{console.error("请求失败：",e)}))};const readEl=document.getElementsByClassName("upload_opt_icon")[0],innerEl=readEl.querySelector(".upload_opt_tip_inner");innerEl.innerHTML="下载",readEl.click();"""
+        self.root.clipboard_clear()  # 清空剪贴板
+        self.root.clipboard_append(scripts)  # 添加文本到剪贴板
+        self.root.attributes("-topmost", True)
 
 
 def start_with_ui():
